950b98de31ed7d27376094752a4e6d9377b298ac
valsimplon <val.simplon@gmail.com>
2017-01-05 10:16:34 +0100

    initialisation nouvelle branche  a partir d'Eclipse apres creation branche en local (vue Git)

---
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -0,0 +1,14 @@
+package co.simplon.factorielle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FactorielleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
