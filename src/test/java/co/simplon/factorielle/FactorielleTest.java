cdc71e5f3965d5bfff18232fe7f74bd30fbe61bf
valsimplon <val.simplon@gmail.com>
2017-01-06 10:36:19 +0100

    integration pde plusieurs annotations @Ignore @Before différence de rendu entre AssertEquals et AssertTrue

--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -2,14 +2,23 @@ package co.simplon.factorielle;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FactorielleTest {
+	
+	public Factorielle factorielle;
 
+	@Before
+	public void initObjet() {
+		factorielle = new Factorielle();
+	}
+	
+	@Ignore
 	@Test
 	public void testCalculerAvecValeur1() {
 		
-		Factorielle factorielle = new Factorielle();
 		long resultat = factorielle.calculer(1);
 		assertEquals(1, resultat);
 
@@ -18,18 +27,15 @@ public class FactorielleTest {
 	@Test
 	public void testCalculerAvecValeur5() {
 		
-		Factorielle factorielle = new Factorielle();
-		
-		assertEquals(120, factorielle.calculer(5));
+		assertTrue(120 == factorielle.calculer(5));
 
 	}
 	
 	@Test
 	public void testCalculerAvecValeurNulle() {
+
 		
-		Factorielle factorielle = new Factorielle();
-		
-		assertEquals(1, factorielle.calculer(0));
+		assertEquals("cas particulier", 1, factorielle.calculer(0));
 
 	}
 	
@@ -45,8 +51,6 @@ public class FactorielleTest {
 	@Test
 	public void testCalculerAvecValeur10() {
 		
-		Factorielle factorielle = new Factorielle();
-		
 		assertEquals(3628800, factorielle.calculer(10));
 
 	}
@@ -54,9 +58,22 @@ public class FactorielleTest {
 	@Test
 	public void testCalculerAvecValeur15() {
 		
-		Factorielle factorielle = new Factorielle();
-		
 		assertEquals(1307674368000l, factorielle.calculer(15));
+
+	}
+	
+	@Ignore	
+	@Test
+	public void testCalculerAvecValeur16() {
+		
+		assertEquals(1307674368000l, factorielle.calculer(16));
+
+	}
+
+	@Test
+	public void testCalculerAvecValeur16Boolean() {
+		
+		assertTrue(1307674368000l == factorielle.calculer(16));
 
 	}
 }
